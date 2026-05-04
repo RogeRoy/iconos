de67f98a3f693e56a8f5509d9385ed8de977bcbb
Rogelio Garcia <roger27049855gmail.com>
2026-05-04 17:27:40 -0600

    Prueba de Commits

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BuilderApp from "./BuilderApp";  
 import BulletinsApp from "./modules/bulletinsm/BulletinsApp";
 
+//Holaaa
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+<Route path="/" element={<ListaBotones />} />
 
-        <Route path="/" element={<ListaBotones />} />
+
+        
 
         {/* BUILDER */}
         <Route path="/builder" element={<BuilderApp />} />
 
       </Routes>
     </BrowserRouter>
-  );
+  ); 
 }
+
